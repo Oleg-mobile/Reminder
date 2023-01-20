d5ea8468c28ea1a8f87be2abbbc08900795b67fe
Oleg Ivanov <juklino@yandex.ru>
2023-01-20 12:42:38 +0300

    - Удалил лишние комментарии + Добавил общую TODO + Создал интерфейс и класс для хранилища

--- Reminder/Models/ReminderTask.cs
+++ Reminder/Models/ReminderTask.cs
@@ -5,7 +5,6 @@ namespace Reminder.Models
     public class ReminderTask
     {
         public Guid Id { get; set; }
-        // TODO public? Конструктор не нужен?
         public string Name { get; set; }
         public DateTime Time { get; set; }
         public string Comment { get; set; }
---
+++ Reminder/Storage/StorageJson.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reminder.Storage
+{
+    public class StorageJson : ITaskStorage
+    {
+        public Task<IEnumerable<string>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
